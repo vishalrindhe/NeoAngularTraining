2f07c9d25f530266a3d50a5bdfd28e34d3683ef4
Vishal Rindhe <rindhevishal32@gmail.com>
2021-03-03 12:42:36 +0530

    Counter complete from LearnCodeOnline

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { stringify } from '@angular/compiler/src/util';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'AngularPrj';
+  title: string = 'AngularPrj';
+  errorMsg!: string;
+  count : number = 0;
+
+  handleIncrease = () =>{
+
+    if(this.count < 5){
+      this.count = this.count +1
+      this.errorMsg  = ""
+    } else{
+      this.errorMsg  = "Count Cannot exceed 5"
+    }
+
+  }
+
+  handleDecrease = () =>{
+    if(this.count <= 0){
+     this.errorMsg  = "Count Cannot go below 0"
+    }else
+    {
+      this.errorMsg  = ""
+      this.count = this.count -1
+     }
+    
+
+  }
+
+  handleReset = () =>{
+    this.count = 0
+  }
 }
